69ba73481e18aa53e3e1610081b7c5ae08122d37
Igoour <barbie-rosa2011@hotmail.com>
2018-08-20 22:20:26 -0300

    Otimização e finalização do Código

--- br.edu.ifc.videira/src/br/edu/ifc/videira/main.java
+++ br.edu.ifc.videira/src/br/edu/ifc/videira/main.java
@@ -1,7 +1,11 @@
 package br.edu.ifc.videira;
 
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+import java.util.List;
+
 public class main {
-public static void main(String[] args){
+	public static void main(String[] args){
 		
 		List<String> compras = new ArrayList<>();
 		List<String> vendas = new ArrayList<>();
@@ -10,7 +14,7 @@ public static void main(String[] args){
 			int op = 0;
 			int op2 = 0;
 			int unid = 0;
-		
+			
 			op = Integer.parseInt(JOptionPane.showInputDialog("Loja de Calçados\n"
 					+ "1 - Comprar produtos\n"
 					+ "2 - Vender produtos\n"
@@ -18,59 +22,56 @@ public static void main(String[] args){
 					+ "4 - Consultar compras\n"
 					+ "5 - Consultar vendas\n"
 					+ "6 - Sair\n"));
-			
-			
-			if(op2==1) {
-				unid = Integer.parseInt(JOptionPane.showInputDialog("Quantidade: \n"));
-				compras.add(unid+" Sapato(s)");
-			}else if(op2==2){
-				unid = Integer.parseInt(JOptionPane.showInputDialog("Quantidade: \n"));
-				compras.add(unid+" Chinelo(s)");
-			}else if(op2==3){
-				unid = Integer.parseInt(JOptionPane.showInputDialog("Quantidade: \n"));
-				compras.add(unid+" Tênis(s)");
-			}else if{
-				unid = Integer.parseInt(JOptionPane.showInputDialog("Quantidade: \n"));
-				compras.add(unid+" Sapatênis(s)");	
-				}}else{
+			if(op==1){
+				op2 = Integer.parseInt(JOptionPane.showInputDialog("1 - Sapato\n"
+						+ "2 - Chinelo\n"
+						+ "3 - Tênis\n"
+						+ "4 - Sapatenis\n"));
+				
+				if(op2==1) {
 					unid = Integer.parseInt(JOptionPane.showInputDialog("Quantidade: \n"));
-					compras.add(unid+" Sandália(s)");	
+					compras.add(unid+" Sapato(s)");
+				}else if(op2==2){
+					unid = Integer.parseInt(JOptionPane.showInputDialog("Quantidade: \n"));
+					compras.add(unid+" Chinelo(s)");
+				}else if(op2==3){
+					unid = Integer.parseInt(JOptionPane.showInputDialog("Quantidade: \n"));
+					compras.add(unid+" Tênis");
+				}else{
+					unid = Integer.parseInt(JOptionPane.showInputDialog("Quantidade: \n"));
+					compras.add(unid+" Sapatenis(s)");	
 					}
-				
-			}else if(op==2){
-					op2 = Integer.parseInt(JOptionPane.showInputDialog("1-Sapato = 180,00R$\n"
-					+ "2- Chinelo = 10,00R$\n"
-					+ "3- Tênis = 80,00R$\n"
-					+ "4- Sapatenis = 70,00R$\n"
-					+ "5- Sandália = 60,00R$\n"));
-			if(op2==1) {
-					vendas.add("Sapato");				
-			}else if(op2==2){
-					vendas.add("Chinelo");	
-			}else if(op2==3){
-					vendas.add("Tênis");	
-			}else if{
-					vendas.add("Sapatenis");	
-			}else{
-					vendas.add("Sandália");	
+				}else if(op==2){
+					op2 = Integer.parseInt(JOptionPane.showInputDialog("1- Sapato = 150,00R$\n"
+							+ "2- Chinelo = 10,00R$\n"
+							+ "3- Tênis = 80,00R$\n"
+							+ "4- Sapatenis = 60,00R$\n"));
+					if(op2==1) {
+						vendas.add("Sapato");				
+					}else if(op2==2){
+						vendas.add("Chinelo");	
+					}else if(op2==3){
+						vendas.add("Tênis");	
+					}else{
+						vendas.add("Sapatenis");	
+					}
+				}else if(op==3){
+						JOptionPane.showMessageDialog(null,"1- Sapato = 130,00R$\n"
+								+ "2- Chinelo = 8,00R$\n"
+								+ "3- Tênis = 70,00R$\n"
+								+ "4- Sapatenis = 50,00R$\n");
+				}else if(op==4) {
+					JOptionPane.showMessageDialog(null, "Vendas: \n"
+							+ "" + compras);	
+				}else if(op==5){
+					JOptionPane.showMessageDialog(null, "Compras: \n"
+							+ "" + vendas);
+				}else{
+					break;
+				}
+		}
 	}
-				
-			}else if(op==3){
-				JOptionPane.showMessageDialog(null,"1- Sapato = 200,00R$\n"
-						+ "2- Chinelo = 15,00R$\n"
-						+ "3- Tênis = 100,00R$\n"
-						+ "4- Sapatenis = 90,00R$\n"
-						+ "5- Sandália = 80,00R$\n");
-		}else if(op==4) {
-			JOptionPane.showMessageDialog(null, "Vendas: \n"
-					+ "" + compras);	
-		}else if(op==5){
-			JOptionPane.showMessageDialog(null, "Compras: \n"
-					+ "" + vendas);
-		}else{
-			break;
-		}
-				
-		}
 }
-}
+		
+	
+
