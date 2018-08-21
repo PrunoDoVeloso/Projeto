bb5012356b2fc79b296bb4a25a5e6e14000930c2
Prunodoveloso <bruno272008@hotmail.com>
2018-08-20 21:47:06 -0300

    Criação do Menu

--- br.edu.ifc.videira/src/br/edu/ifc/videira/main.java
+++ br.edu.ifc.videira/src/br/edu/ifc/videira/main.java
@@ -10,6 +10,14 @@ public static void main(String[] args){
 			int op = 0;
 			int op2 = 0;
 			int unid = 0;
+		
+			op = Integer.parseInt(JOptionPane.showInputDialog("Loja de Calçados\n"
+					+ "1 - Comprar produtos\n"
+					+ "2 - Vender produtos\n"
+					+ "3 - Lista de preços\n"
+					+ "4 - Consultar compras\n"
+					+ "5 - Consultar vendas\n"
+					+ "6 - Sair\n"));
 		}
 }
 }
