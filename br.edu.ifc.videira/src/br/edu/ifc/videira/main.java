a293df732d8297f8c3ae1fdf5d8fd420284d4365
Prunodoveloso <bruno272008@hotmail.com>
2018-08-20 21:36:11 -0300

    Criação de Listas e Variáveis

--- br.edu.ifc.videira/src/br/edu/ifc/videira/main.java
+++ br.edu.ifc.videira/src/br/edu/ifc/videira/main.java
@@ -1,5 +1,15 @@
 package br.edu.ifc.videira;
 
 public class main {
-
+public static void main(String[] args){
+		
+		List<String> compras = new ArrayList<>();
+		List<String> vendas = new ArrayList<>();
+		
+		for(;;) {
+			int op = 0;
+			int op2 = 0;
+			int unid = 0;
+		}
+}
 }
